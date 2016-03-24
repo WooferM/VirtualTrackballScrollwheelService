cfeaee269f6d357d1fa8ec08bad1d27ed00c65d0
Michael Townrow <WooferM@users.noreply.github.com>
2016-03-23 22:31:34 -0700

    Initial Version

---
+++ README.md
@@ -0,0 +1,29 @@
+Virtual Trackball Scrollwheel Service
+===============
+**Allows scrolling with a trackball or other pointing device without a scroll wheel by using a low level mouse hook.**
+
+Changes the behaviour of any X-Button (typically buttons 3 and 4) and when both left and right mouse buttons are held to scroll. i.e. vertical mouse wheel events, when an X-Button or both left and right buttons are pressed and the trackball is moved
+
+Based on https://github.com/Seelge/TrackballScroll
+
+###### Requirements
+- A trackball or mouse
+- A Microsoft Windows operating system
+
+This software has been tested with a *Logitech Marge Mouse* (tm) (2 button) and *Logitech Trackman Marble* (tm) in *Microsoft Windows 7*
+
+###### Download the latest release
+
+
+###### Run the program
+- Execute `VirtualTrackballScrollwheelService.exe`, no installation is necessary.
+- Add to the Startup folder in your start menu to run automatically on boot.
+- When using this program with a driver software that allows customization of the button behavior, make sure to set the X-Buttons to default behavior. E.g. with a *Logitech Trackman Marble*, make sure to set the buttons 3 and 4 to `default` button behaviour or `Generic Button` and not `back`/`Universal Scroll`.
+- The lines scrolled per wheel event are determined by the *Microsoft Windows* mouse wheel settings.
+
+###### Compile the source code
+- Clone the repository from the github page or download the latest source code archive
+- Open the solution with *Microsoft Visual Studio 2010*. If you are using a different version, create a new solution and add the missing files.
+- Change the build type to `Release` and `x64`. The program also works in both 64 and 32 bit programs.
+- Build the solution
+- `VirtualTrackballScrollwheelService.exe` will be created in the `\Release` folder 
---
+++ VirtualTrackballScrollwheelService/VirtualTrackballScrollwheelService.cpp
@@ -0,0 +1,227 @@
+// VirtualTrackballScrollwheelService.cpp : Implementation of WinMain
+
+
+#include "stdafx.h"
+#include "resource.h"
+#include "VirtualTrackballScrollwheelService_i.h"
+
+
+#include <stdio.h>
+
+class CVirtualTrackballScrollwheelServiceModule : public ATL::CAtlServiceModuleT< CVirtualTrackballScrollwheelServiceModule, IDS_SERVICENAME >
+	{
+public :
+	DECLARE_LIBID(LIBID_VirtualTrackballScrollwheelServiceLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_VIRTUALTRACKBALLSCROLLWHEELSERVICE, "{0B7B685E-051E-4641-88FD-F85575F89177}")
+		HRESULT InitializeSecurity() throw()
+	{
+		// TODO : Call CoInitializeSecurity and provide the appropriate security settings for your service
+		// Suggested - PKT Level Authentication, 
+		// Impersonation Level of RPC_C_IMP_LEVEL_IDENTIFY 
+		// and an appropiate Non NULL Security Descriptor.
+
+		return S_OK;
+	}
+	};
+
+CVirtualTrackballScrollwheelServiceModule _AtlModule;
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;								// current instance
+TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+const int Y_THRESHOLD = 10;   // threshold in pixels to trigger wheel event
+const DWORD WHEEL_FACTOR = 1; // number of wheel events. The lines scrolled per wheel event are determined by the Microsoft Windows mouse wheel settings.
+enum State {
+	NORMAL = 0, // default state
+	DOWN,       // mouse XButton pressed, no movement
+	SCROLL,     // mouse XButton pressed + moved
+	L_DOWN,		// left mouse button pressed
+	R_DOWN,		// right mouse button pressed
+	LR_DOWN,	// left and right mouse button pressed
+	LR_SCROLL,	// left and right mouse button pressed + moved
+};
+HHOOK g_Hook;           // callback function hook, called for every mouse event
+State g_state = NORMAL; // initial state
+POINT g_origin;         // cursor position when entering state DOWN
+int g_ycount = 0;       // accumulated vertical movement while in state SCROLL
+
+LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
+	if (nCode != HC_ACTION)
+		return CallNextHookEx(g_Hook, nCode, wParam, lParam);
+	BOOL preventCallNextHookEx = FALSE;
+	const MSLLHOOKSTRUCT *p = reinterpret_cast<const MSLLHOOKSTRUCT*>(lParam);
+	switch (g_state) {
+	case NORMAL:
+		if (wParam == WM_XBUTTONDOWN) { // NORMAL->DOWN: remember position
+			preventCallNextHookEx = TRUE;
+			g_state = DOWN;
+			g_origin = p->pt;
+		} else if (wParam == WM_LBUTTONDOWN) { // NORMAL->L_DOWN: remember position
+			preventCallNextHookEx = FALSE;
+			g_state = L_DOWN;
+			g_origin = p->pt;
+		} /*else if (wParam == WM_RBUTTONDOWN) { // NORMAL->R_DOWN: remember position
+			preventCallNextHookEx = TRUE;
+			g_state = R_DOWN;
+			g_origin = p->pt;
+		}*/
+		break;
+	case DOWN:
+		/*if (wParam == WM_XBUTTONUP) { // DOWN->NORMAL: middle button click
+			preventCallNextHookEx = TRUE;
+			g_state = NORMAL;
+			INPUT input[2];
+			input[0].type = INPUT_MOUSE;
+			input[0].mi.dx = p->pt.x;
+			input[0].mi.dy = p->pt.y;
+			input[0].mi.mouseData = (DWORD)0x0;
+			input[0].mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN; // middle button down
+			input[0].mi.time = (DWORD)0x0;
+			input[0].mi.dwExtraInfo = (ULONG_PTR)NULL;
+			input[1].type = INPUT_MOUSE;
+			input[1].mi.dx = p->pt.x;
+			input[1].mi.dy = p->pt.y;
+			input[1].mi.mouseData = (DWORD)0x0;
+			input[1].mi.dwFlags = MOUSEEVENTF_MIDDLEUP; // middle button up
+			input[1].mi.time = (DWORD)0x0;
+			input[1].mi.dwExtraInfo = (ULONG_PTR)NULL;
+			SendInput(2, input, sizeof(INPUT));
+		}
+		else */ 
+		if (wParam == WM_MOUSEMOVE) { // DOWN->SCROLL
+			preventCallNextHookEx = TRUE;
+			g_state = SCROLL;
+			g_ycount = 0;
+			SetCursorPos(g_origin.x, g_origin.y);
+		}
+		break;
+	case SCROLL:
+		if (wParam == WM_XBUTTONUP) { // SCROLL->NORMAL
+			preventCallNextHookEx = TRUE;
+			g_state = NORMAL;
+		}
+		else if (wParam == WM_MOUSEMOVE) { // SCROLL->SCROLL: wheel event
+			preventCallNextHookEx = TRUE;
+			g_ycount += p->pt.y - g_origin.y;
+			SetCursorPos(g_origin.x, g_origin.y);
+			if (g_ycount < -Y_THRESHOLD || g_ycount > Y_THRESHOLD){
+				DWORD mouseData = (g_ycount > 0 ? -WHEEL_DELTA : +WHEEL_DELTA); // scroll direction
+				g_ycount = 0;
+				INPUT input[WHEEL_FACTOR];
+				for (size_t i = 0; i < WHEEL_FACTOR; ++i) {
+					input[i].type = INPUT_MOUSE;
+					input[i].mi.dx = p->pt.x;
+					input[i].mi.dy = p->pt.y;
+					input[i].mi.mouseData = mouseData;
+					input[i].mi.dwFlags = MOUSEEVENTF_WHEEL; // wheel
+					input[i].mi.time = (DWORD)0x0;
+					input[i].mi.dwExtraInfo = (ULONG_PTR)NULL;
+				}
+				SendInput(WHEEL_FACTOR, input, sizeof(INPUT));
+			}
+		}
+		break;
+	case L_DOWN:
+		 if (wParam == WM_RBUTTONDOWN) { // L_DOWN-> LR_DOWN: remember position
+			preventCallNextHookEx = TRUE;
+			g_state = LR_DOWN;
+			g_origin = p->pt;
+		} else if (wParam == WM_LBUTTONUP) { // L_DOWN->NORMAL
+			preventCallNextHookEx = FALSE;
+			g_state = NORMAL;
+		}
+
+		break;
+	/*case R_DOWN:
+		if (wParam == WM_LBUTTONDOWN) { // R_DOWN->LR_DOWN: remember position
+			preventCallNextHookEx = FALSE;
+			g_state = LR_DOWN;
+			g_origin = p->pt;
+		} else if (wParam == WM_RBUTTONUP) { // R_DOWN ->NORMAL
+			preventCallNextHookEx = FALSE;
+			g_state = NORMAL;
+		} 
+		break;
+		*/
+	case LR_DOWN:
+		if (wParam == WM_MOUSEMOVE) { // LR_DOWN->LR_SCROLL
+			preventCallNextHookEx = TRUE;
+			g_state = LR_SCROLL;
+			g_ycount = 0;
+			SetCursorPos(g_origin.x, g_origin.y);
+		} else if (wParam == WM_LBUTTONUP) { // LR_DOWN->NORMAL
+			preventCallNextHookEx = FALSE;
+			g_state = NORMAL;
+		}
+		else if (wParam == WM_RBUTTONUP) { // LR_DOWN-> L_DOWN
+			preventCallNextHookEx = TRUE;
+			g_state = L_DOWN;
+		}
+		break;
+	case LR_SCROLL:
+		if (wParam == WM_LBUTTONUP) { // LR_SCROLL->NORMAL
+			preventCallNextHookEx = TRUE;
+			g_state = NORMAL;
+		}
+		else if (wParam == WM_RBUTTONUP) { // LR_SCROLL-> L_DOWN
+			preventCallNextHookEx = TRUE;
+			g_state = L_DOWN;
+		}
+		else if (wParam == WM_MOUSEMOVE) { // SCROLL->SCROLL: wheel event
+			preventCallNextHookEx = TRUE;
+			g_ycount += p->pt.y - g_origin.y;
+			SetCursorPos(g_origin.x, g_origin.y);
+			if (g_ycount < -Y_THRESHOLD || g_ycount > Y_THRESHOLD){
+				DWORD mouseData = (g_ycount > 0 ? -WHEEL_DELTA : +WHEEL_DELTA); // scroll direction
+				g_ycount = 0;
+				INPUT input[WHEEL_FACTOR];
+				for (size_t i = 0; i < WHEEL_FACTOR; ++i) {
+					input[i].type = INPUT_MOUSE;
+					input[i].mi.dx = p->pt.x;
+					input[i].mi.dy = p->pt.y;
+					input[i].mi.mouseData = mouseData;
+					input[i].mi.dwFlags = MOUSEEVENTF_WHEEL; // wheel
+					input[i].mi.time = (DWORD)0x0;
+					input[i].mi.dwExtraInfo = (ULONG_PTR)NULL;
+				}
+				SendInput(WHEEL_FACTOR, input, sizeof(INPUT));
+			}
+		}
+		break;
+	}
+	return (preventCallNextHookEx ? 1 : CallNextHookEx(g_Hook, nCode, wParam, lParam));
+}
+
+//
+extern "C" int WINAPI _tWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, 
+								LPTSTR /*lpCmdLine*/, int nShowCmd)
+{
+		MSG msg;
+	HACCEL hAccelTable;
+
+//attach VirtualTrackballScrollwhell hook
+	g_Hook = SetWindowsHookEx(WH_MOUSE_LL, &LowLevelMouseProc, GetModuleHandle(0), 0);
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	UnhookWindowsHookEx(g_Hook); // remove hook	return _AtlModule.WinMain(nShowCmd);
+}
+
